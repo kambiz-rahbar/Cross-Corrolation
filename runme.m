6a8df567d60412528c89c9e5d098bc433d3f9b26
ANCHA <45118134+kambiz-rahbar@users.noreply.github.com>
2019-10-15 23:54:26 +0330

    Add files via upload

--- runme.m
+++ runme.m
@@ -2,8 +2,17 @@ clc
 clear
 close all
 
-x = rand(100,1);
-y = rand(50,1);
+x = rand(3,1);
+y = rand(2,1);
 
+
+% test auto correlation
+% [rxx,~,~] = my_auto_corr(x);
+% disp('matlab results:'); disp(xcorr(x,x)')
+% disp('my results:'); disp(rxx')
+
+
+% test cross correlation
 [r,normalized_r] = my_cross_corr(x,y);
-
+disp('matlab results:'); disp(xcorr(x,y)')
+disp('my results:'); disp(r')
