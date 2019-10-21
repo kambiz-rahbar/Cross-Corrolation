01237a8f0935fa13e7c2478cfee229c13365cf35
ANCHA <45118134+kambiz-rahbar@users.noreply.github.com>
2019-10-21 11:35:56 +0330

    Add files via upload

--- my_cross_corr.m
+++ my_cross_corr.m
@@ -3,15 +3,15 @@ function [r,normalized_r] = my_cross_corr(x,y)
 [~,~,rx0] = my_auto_corr(x);
 [~,~,ry0] = my_auto_corr(y);
 
-% make change to calculate corr with conv
+y = flip(y);
 r = conv(x,y);
 
 normalized_r = r ./sqrt(rx0*ry0);
 
-
 tx = 1:length(x);
 ty = 1:length(y);
-tr = ceil(-(length(x)+length(y)-1)/2) :  floor((length(x)+length(y)-1)/2);
+tr = 1: length(x)+length(y)-1;
+tr = tr - ceil(length(tr)/2);
 
 figure;
 subplot(2,2,1); stem(tx,x); title('x');
